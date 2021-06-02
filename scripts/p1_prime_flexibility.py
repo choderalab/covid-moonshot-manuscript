415eadd4ab14e8bc8ca0d8d2a97d9cd5c5b47b30
William Glass <wglass149@googlemail.com>
2021-06-02 10:42:57 +0100

    add mpro pocket flexibility scripts

---
+++ scripts/p1_prime_flexibility.py
@@ -0,0 +1,110 @@
+import pymol
+from pymol import cmd, util
+from sys import argv
+
+# usage: pymol -cq script_name.py -- path/to/fragments/
+
+path = argv[1:][0]
+print(f"Using path: {path} for structures")
+
+# Set some nice CB friendly colours
+cmd.set_color("cb_orange", [0.96, 0.41, 0.23])
+
+# Add filter (ambient) and other misc settings
+cmd.set("ambient", 0.4)
+cmd.set("ambient_occlusion_mode", 1)
+cmd.set("ambient_occlusion_scale", 15)
+cmd.bg_colour("white")
+cmd.set("antialias", 2)
+cmd.set("ortho", 1)
+cmd.set("ray_trace_mode", 0)
+
+cmd.reset()
+cmd.delete('all')
+
+# Specify fragment codes from each series
+amino_p1_prime = "Mpro-x10322_0A"
+ugi_p1_prime = "Mpro-x2694_0A"
+quin_p1_prime = "Mpro-P0008_0B"
+benzo_p1_prime = "Mpro-x12136_0A"
+
+fragments = [amino_p1_prime, ugi_p1_prime, quin_p1_prime, benzo_p1_prime] 
+
+# Load each fragment: ligand + apo protein
+for i, fragment in enumerate(fragments):
+  cmd.load(path + f'{fragment}/' + f'{fragment}.sdf', f'p1prime-{i}-{fragment}-ligand')
+  cmd.load(path + f'{fragment}/' + f'{fragment}_apo-desolv.pdb', f'p1prime-{i}-{fragment}-protein')
+
+# Sort out colours for each series
+cmd.color("wheat", f"p1prime-0-*") # amino
+cmd.color("palegreen", f"p1prime-1-*") # ugi
+cmd.color("violet", f"p1prime-2-*") # quin
+cmd.color("deepolive", f"p1prime-3-*") # benzo
+
+# Retain non-carbon default colours
+util.cnc(f"p1prime-*")
+
+# remove waters
+cmd.remove('resn HOH')
+cmd.deselect()
+
+# Show molecular representation
+cmd.hide('all')
+cmd.bg_color('white')
+util.cbaw('*-protein')
+
+cmd.show('sticks', f'*-protein and not hydrogen')
+cmd.show('surface', f'*-protein and not hydrogen')
+cmd.show('sticks', 'not hydrogen and *-ligand')
+
+cmd.hide('sticks', 'hydrogen')
+
+cmd.disable('*-protein')
+cmd.disable('*-ligand')
+
+cmd.set('surface_color', 'white')
+
+# Sort transparency for surfaces and catalytic dyad
+cmd.set('surface_mode', 3)
+cmd.set('transparency', 0.15)
+cmd.set('transparency', 1, 'resi 145 and resn CYS')
+cmd.set('transparency', 1, 'resi 41 and resn HIS')
+
+# Set the viewport and view
+cmd.viewport(720,720)
+cmd.set_view ("\
+     0.889428616,   -0.456408978,    0.024401871,\
+    -0.099697880,   -0.141634151,    0.984882414,\
+    -0.446053863,   -0.878418088,   -0.171478689,\
+     0.000357639,    0.000210542,  -94.736244202,\
+   -21.802495956,    4.796146870,   28.488866806,\
+    81.561714172,  107.897544861,   20.000000000 ")
+
+# Label MPro pockets
+cmd.set("label_shadow_mode", 2)
+
+# P1 prime
+cmd.select("p1_prime", "resi 25+26+27")
+cmd.set("surface_color", "cb_orange", "p1_prime")
+cmd.show("surface", "p1_prime")
+cmd.pseudoatom("p1_prime_label", "p1_prime")
+cmd.set("label_color", "cb_orange", "p1_prime_label")
+cmd.set("label_size", -0.8, "p1_prime_label")
+cmd.set("label_font_id", 7, "p1_prime_label")
+### hide psuedoatom
+cmd.hide("everything", "p1_prime_label")
+cmd.show("label", "p1_prime_label")
+
+pymol.finish_launching()
+
+# Create images
+for i, fragment in zip([0,1,2,3], fragments):
+    print(i, fragment)
+    cmd.enable(f'p1prime-{i}-{fragment}-ligand')
+    cmd.enable(f'p1prime-{i}-{fragment}-protein')
+
+    cmd.ray(720,720)
+    cmd.png(f"./p1_prime_flex_{i}.png")
+    
+    cmd.disable(f'p1prime-{i}-{fragment}-ligand')
+    cmd.disable(f'p1prime-{i}-{fragment}-protein')
