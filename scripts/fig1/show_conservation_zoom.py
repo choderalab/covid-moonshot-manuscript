78b8f9c4d843749fd0002fe910a59769b4fbac43
William Glass <wglass149@googlemail.com>
2021-06-16 15:25:06 +0100

    add conservation scripts

--- scripts/fig1/show_conservation_zoom.py
+++ scripts/fig1/show_conservation_zoom.py
@@ -25,6 +25,9 @@ cmd.set("antialias", 2)
 cmd.set("ortho", 1)
 cmd.set("ray_trace_mode", 0)
 
+# use a white background
+cmd.set("ray_opaque_background", "on")
+
 cmd.set("surface_smooth_edges", "on")
 
 cmd.reset()
@@ -45,35 +48,52 @@ cmd.deselect()
 
 # Show molecular representation
 cmd.hide("all")
-# cmd.dss(f"{pdb_name}")
 cmd.bg_color("white")
 util.cbaw("*-protein")
 
 # colour based on bfactor
 cmd.spectrum("b", "magenta_white", "*-protein", minimum=0, maximum=1)
 
+# TODO: simplify this selection string
+cmd.show(
+    "sticks",
+    f"*-protein and not hydrogen and not name N and not name C and not name O and not resn 02J and not resn PJE and not resn 010 and not resn PRO and b<1",
+)
+cmd.show(
+    "sticks",
+    f"*-protein and not hydrogen and resn PRO and not name C and not name O and b<1",
+)
+
+cmd.show("cartoon", f"*-protein and not hydrogen")
 cmd.show("surface", f"*-protein and not hydrogen")
-# cmd.show("ribbon", f"*-protein and not hydrogen")
 
 cmd.disable("*-protein")
 cmd.enable(f'methoxy_benzo-{methoxy_benzo_fragment}-protein')
 
+# cmd.set("surface_color", "white")
+
+# molecule = "N3"
 cmd.show("sticks", f"methoxy_benzo-{methoxy_benzo_fragment}-ligand and not hydrogen")
 
 cmd.set("surface_mode", 3)
 
+# Sort transparency for surfaces and catalytic dyad
+cmd.set("transparency", 0.25)
+# cmd.set("transparency", 1, "resi 145 and resn CYS")
+# cmd.set("transparency", 1, "resi 41 and resn HIS")
+
 # Set the viewport and view
 cmd.viewport(720, 720)
-cmd.set_view("\
+cmd.set_view ("\
      0.885667682,   -0.423480660,   -0.190375701,\
     -0.004551230,   -0.417925209,    0.908467293,\
     -0.464280874,   -0.803735554,   -0.372073978,\
-     0.000368759,    0.000287153, -224.885543823,\
-   -10.193267822,    6.786962986,   27.136997223,\
-    34.983333588,  414.772888184,   20.000000000 ")
+     0.000575241,    0.000248406,  -94.735580444,\
+   -23.217260361,    4.976941109,   29.377637863,\
+    81.561714172,  107.897544861,   20.000000000 ")
 
 pymol.finish_launching()
 
 # Create image
 cmd.ray(720, 720)
-cmd.png("mpro_conservation.png", dpi=300)
+cmd.png("mpro_pocket_conservation.png", dpi=300)
