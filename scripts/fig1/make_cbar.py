22f3077038c9f68b5f9b881cdae749e747e5eccd
William Glass <wglass149@googlemail.com>
2021-07-13 16:24:32 +0100

    use vertical colour bar

--- scripts/fig1/make_cbar.py
+++ scripts/fig1/make_cbar.py
@@ -4,18 +4,25 @@ import matplotlib as mpl
 from matplotlib.colors import LinearSegmentedColormap
 
 
-fig, ax = plt.subplots(figsize=(6, 1.5))
-fig.subplots_adjust(bottom=0.5)
+# fig, ax = plt.subplots(figsize=(6, 1.5))
+# fig.subplots_adjust(bottom=0.5)
+
+fig, ax = plt.subplots(figsize=(1.75, 6))
+# fig.subplots_adjust(bottom=0.5)
 
 c = ["magenta", "white"]
 cmap = LinearSegmentedColormap.from_list("custom_cmap", c)
 norm = mpl.colors.Normalize(vmin=0, vmax=1)
 
+# cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+#              cax=ax, orientation='horizontal')
+
 cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-             cax=ax, orientation='horizontal')
+             cax=ax, orientation='vertical')
 
 cbar.set_ticks([0,1])
-cbar.ax.set_xticklabels(['Low', 'High'], size=18)
+# cbar.ax.set_xticklabels(['Low', 'High'], size=18)
+cbar.ax.set_yticklabels(['Low', 'High'], size=18)
 cbar.set_label(label='Conservation',size=22)
 
 plt.tight_layout()
