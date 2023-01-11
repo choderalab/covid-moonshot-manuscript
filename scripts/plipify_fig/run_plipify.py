e5ddac130b84b7fffd62840d3dbbbf237b527121
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-01-10 19:06:39 -0500

    copy old script to use pathlib

--- scripts/plipify_fig/run_plipify.py
+++ scripts/plipify_fig/run_plipify.py
@@ -4,6 +4,7 @@ Assumes the provided glob points to a set of PDB structures with a single ligand
 Written in the style of asapdiscovery repo scripts `https://github.com/choderalab/covid-moonshot-ml`
 """
 import argparse, os, sys
+from pathlib import Path
 from glob import glob
 from tqdm import tqdm
 import shutil
@@ -38,20 +39,34 @@ def main():
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    structure_fns = [fn for fn in glob(args.input_glob)]
-    # print(structure_fns)
+    TARGET = "Mpro"
+
+    HERE = Path(os.getcwd())
+    DATA = HERE / "data" / TARGET
+    OUT = Path(args.output_dir)
+    DATA.mkdir(exist_ok=True, parents=True)
+    OUT.mkdir(exist_ok=True, parents=True)
+
+    pdbs = list((DATA / "aligned").glob("**/*_bound_chain*.pdb"))
+    # print(pdbs)
     ## for debugging
-    structure_fns = structure_fns[0:100]
-    structure_labels = [os.path.basename(os.path.dirname(fn)) for fn in structure_fns]
+    # pdbs = pdbs[0:200]
     # print(structure_labels)
 
     ## make new filenames and load structure objects
     structures = []
-    for fn, label in tqdm(zip(structure_fns, structure_labels), total=len(structure_fns)):
-        new_fn = os.path.join(args.output_dir, f"{label}.pdb")
+    for path in tqdm(pdbs, total=len(pdbs)):
+        new_fn = OUT / Path(f"{path.parent.name}.pdb")
         if not os.path.exists(new_fn):
-            shutil.copy(fn, new_fn)
-        structures.append(Structure.from_pdbfile(str(new_fn), ligand_name="LIG"))
+            shutil.copy(path, new_fn)
+        structure = Structure.from_pdbfile(str(new_fn), ligand_name="LIG")
+
+        ## Skip structures with multiple binding sites
+        if len(structure.binding_sites) != 1:
+            print(
+                f"{path.relative_to(HERE)} contains {len(structure.binding_sites)} binding sites and we want exactly one.")
+            continue
+        structures.append(structure)
     print(f"Loaded {len(structures)} structures.")
 
     ## A bunch of filtering
@@ -61,7 +76,7 @@ def main():
 
     lengths = lengths[(lengths.length - lengths.length.median()).abs() < lengths.length.std()]
     filtered_structures = [s for s in structures if s.identifier in set(lengths.identifier.tolist())]
-    print(len(structure_fns), "->", len(structures), "->", len(filtered_structures), "=", len(structure_fns) - len(filtered_structures),
+    print(len(pdbs), "->", len(structures), "->", len(filtered_structures), "=", len(pdbs) - len(filtered_structures),
           "structures filtered out")
 
     structures = filtered_structures
@@ -70,7 +85,6 @@ def main():
     print(structure_name_type_dict.keys())
     for s in structures:
         name_code = s.identifier[0:6]
-        print(name_code)
         if name_code in structure_name_type_dict.keys():
             structure_name_type_dict[name_code] += 1
         else:
